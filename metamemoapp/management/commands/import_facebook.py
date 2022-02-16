65a252f382176805bc2922c836a202052dcbd6a4
Pedro Markun <pedro@markun.com.br>
2022-02-16 10:39:16 -0300

    refatorando import_facebook.py nao esta baixando posts c/ video #TOFIX

--- metamemoapp/management/commands/import_facebook.py
+++ metamemoapp/management/commands/import_facebook.py
@@ -1,30 +1,50 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.core.serializers.json import DjangoJSONEncoder
+
 from django.utils import timezone
 from metamemoapp.models import MetaMemo, MemoItem, MemoSource
-import csv
-import json
+import json, pprint, os
+import facebook_scraper
 
 """
 Cada arquivo dentro de management/commands é um comando que pode ser rodado usando 'python manage.py nomedocomando'.
 Handle() é a função executada pelo código.
 A vantagem de fazer assim é que as libs e dependencias do Django já estão encapsuladas e você pode chamar essa função dentro do resto do código.
 """
+    
 
 class Command(BaseCommand):
     help = 'Importa de um arquivo de facebook'
 
     def add_arguments(self, parser):
-        parser.add_argument('-f', '--filename', type=str, help='CSV File to be imported')
+        parser.add_argument('-u', '--username', type=str, help='Facebook Username')
         parser.add_argument('-a', '--author', type=str, help='MetaMemo Author Name')
+        parser.add_argument('-c', '--clear', action='store_true')
 
 
     def handle(self, *args, **kwargs):
-        filename = kwargs['filename']
+        username = kwargs['username']
         author = kwargs['author']
+        clear = kwargs['clear']
         
         memo_author = MetaMemo.objects.get_or_create(name=author)
         memo_source = MemoSource.objects.get_or_create(name='Facebook')
-        input_file = csv.DictReader(open(filename, 'r'))
+        
+        if not clear:
+            memo_itens = MemoItem.objects.filter(author__name=author, source__name='Facebook').values_list('original_id', flat=True)
+        else:
+            memo_itens = []
+
+
+        cookies_file = getattr(settings, "FACEBOOK_COOKIES", None)
+        pages = getattr(settings, "FACEBOOK_PAGES", 4)
+        ppp = getattr(settings, "FACEBOOK_PPP", 10)
+        
+        if not cookies_file or not os.path.isfile(cookies_file):
+            print("Facebook Cookie File not found!")
+            raise
+        input_posts = facebook_scraper.get_posts(username, pages=pages, options={'posts_per_page':ppp}, cookies=cookies_file)
         
         """
         Aqui estou usando o MemoItem() para instancear um objeto que defini lá no models.
@@ -33,16 +53,30 @@ class Command(BaseCommand):
 
         """
         
-        for i in input_file:
-            post = MemoItem()
-            post.author = memo_author[0]
-            post.source = memo_source[0]
-            post.title = i['post_text'][0:139]
-            post.content = i['post_text']
-            post.extraction_date = '2022-01-20 00:00:00'
-            post.content_date = i['time']
-            post.url = i['post_url']
-            post.likes = i['likes']
-            post.interactions = i['comments']
-            post.raw = json.dumps(i)
-            post.save()
+        for i in input_posts:
+            if i['post_id'] in memo_itens:
+                print("Done!")
+                break
+            else:
+                pprint.pprint(i, indent=3)
+                post = MemoItem()
+                post.author = memo_author[0]
+                post.source = memo_source[0]
+                if i['text']:
+                    post.title = i['text'][0:139].replace('\n',' ')
+                else:
+                    post.title = "" #FIX
+                post.content = i['post_text']
+                post.extraction_date = '2022-01-20 00:00:00' #FIX
+                post.content_date = i['time']
+                post.url = i['post_url']
+                post.likes = i['likes']
+                post.interactions = i['comments']
+                post.original_id = i['post_id']
+                post.raw = json.dumps(i, sort_keys=True, indent=1, cls=DjangoJSONEncoder)
+                post.save()
+                
+                if i['video']:
+                    post.medias.create(original_url=i['video'], original_id=['video_id'], status='INITIAL')
+
+        
