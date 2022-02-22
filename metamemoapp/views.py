a45969b89729321e8f7b7226d86e906df6a02e41
Pedro Markun <pedro@markun.com.br>
2022-02-22 14:40:50 -0300

    data gte

--- metamemoapp/views.py
+++ metamemoapp/views.py
@@ -9,7 +9,7 @@ def index(request):
 def list(request, day=datetime.now().day, month=datetime.now().month, year=datetime.now().year):
     d = datetime(year, month,day)
     memoitem_list = MemoItem.objects.filter(content_date__day=day,content_date__month=month,content_date__year=year)
-    memocontext_list = MemoContext.objects.filter(start_date__day__lte=day,start_date__month__lte=month,start_date__year__lte=year)
+    memocontext_list = MemoContext.objects.filter(start_date__day__lte=day,start_date__month__lte=month,start_date__year__lte=year, end_date__day__gte=day,end_date__month__gte=month,end_date__year__gte=year)
     memonews_list = MemoNews.objects.filter(content_date__day=day,content_date__month=month,content_date__year=year)
 
     return render(request, 'list.html', {'date':d, 'memoitem_list': memoitem_list, 'memocontext_list': memocontext_list, 'memonews_list':memonews_list})
