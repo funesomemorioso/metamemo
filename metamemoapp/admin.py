51af9cb233fb17023afbe03948bd5b878ad1d9ef
Pedro Markun <pedro@markun.com.br>
2022-03-05 12:57:51 -0300

    kwargs ao inves de args no admin

--- metamemoapp/admin.py
+++ metamemoapp/admin.py
@@ -25,7 +25,7 @@ Vale ler a documentação.
 #Action para baixar as midias. TODO: Precisa jogar o job prum Celery da vida.
 def download_media(modeladmin, request, queryset):
     for i in queryset.filter(status='INITIAL', mediatype='VIDEO'):
-        download_async.delay(i.pk)
+        download_async.delay(pk=i.pk)
         i.status = 'DOWNLOADING'
         i.save()
         messages.add_message(request, messages.SUCCESS, 'Download job started.')
@@ -34,7 +34,7 @@ download_media.short_description = 'Download Video Media'
 
 def transcribe_media(modeladmin, request, queryset):
     for i in queryset.filter(status='DOWNLOADED', mediatype='VIDEO'):
-        transcribe_async.delay(i.pk)
+        transcribe_async.delay(pk=i.pk)
         i.status = 'TRANSCRIBING'
         i.save()
         messages.add_message(request, messages.SUCCESS, 'Transcription job started.')
