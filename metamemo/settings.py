9dfc6ab38dca6b8b3cb936059a59d54c5cf6ba99
Pedro Markun <pedro@markun.com.br>
2022-03-07 11:57:11 -0300

    dynamic path para o STATIC ROOT

--- metamemo/settings.py
+++ metamemo/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/markun/devel/metamemo/metamemo/static'
+STATIC_ROOT = str(BASE_DIR) + '/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
