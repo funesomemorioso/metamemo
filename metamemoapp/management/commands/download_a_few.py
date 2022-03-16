bf71dcefb05d0db2707e2f20fc423da773032e06
Pedro Markun <pedro@markun.com.br>
2022-03-15 21:45:42 -0300

    helper function pra ir fazendo devagar

---
+++ metamemoapp/management/commands/download_a_few.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand
+
+from metamemoapp.models import MemoMedia
+from metamemoapp.tasks import download_img_async, download_async
+
+class Command(BaseCommand):
+    help = 'Download media from a few random medias'
+
+    def add_arguments(self, parser):
+        parser.add_argument('-n', '--number', type=int, help='How many to download?')
+        parser.add_argument('-t', '--type', type=str, help='Which types?')
+        parser.add_argument('-f', '--fake', action='store_true')
+        
+
+    def handle(self, *args, **kwargs):
+        self.count = kwargs['number']
+        self.type = kwargs['type']
+        self.fake = kwargs['fake']
+        
+        self.medias = MemoMedia.objects.filter(media='').exclude(status='DOWNLOADING')
+        if self.type:
+            self.medias = self.medias.filter(mediatype=self.type)
+        
+        for p in self.medias[0:self.count]:
+            print(f'Downloading {p.pk} {p.mediatype} from {p.original_url}')
+            print(p.media)
+            if p.mediatype == 'VIDEO' and not self.fake:
+                p.status = 'DOWNLOADING'
+                p.save()
+                download_async.apply_async(kwargs={'url': p.original_url, 'mediatype': 'VIDEO'})
+            elif p.mediatype=='IMAGE' and not self.fake:
+                download_img_async.apply_async(kwargs={'url' : p.original_url, 'pk' : p.pk})
