14eced6716e385da7f6327143e453c41cc3f9329
Pedro Markun <pedro@markun.com.br>
2022-04-23 13:27:04 -0300

    atualizando script folha, filtro por palavras no título

--- metamemoapp/management/commands/import_folha.py
+++ metamemoapp/management/commands/import_folha.py
@@ -18,6 +18,14 @@ class Command(BaseCommand):
     sr = 1
     site = 'jornal'
 
+    def checkWords(self, list_words, text):
+        if not list_words:
+            return True
+        for word in list_words.split(","):
+            if word.lower() in text.lower():
+                return True
+        return False
+
     def scrapeFolha(self, keyword):
         all_news = NewsItem.objects.filter(source=self.source, metamemo=self.metamemo[0]).values_list('url', flat=True)
         
@@ -26,40 +34,42 @@ class Command(BaseCommand):
         soap = soap.read()
         soap = html.fromstring(soap)
         for n in soap.xpath("//li[contains(@class,'c-headline')]"):
-            try:
-                
-                news = NewsItem()
-                news.title = n.xpath(".//h2[contains(@class,'c-headline__title')]")[0].text.strip()
-                news.text = n.xpath(".//p[contains(@class, 'c-headline__standfirst')]")[0].text_content().strip()
-                news.url = n.xpath(".//div[contains(@class, 'c-headline__content')]/a")[0].get("href")
-                dt = n.xpath(".//time")[0].text_content().strip().replace('º','')
-                news.content_date = parseDate(dt)
-                news.metamemo = self.metamemo[0]
-                news.source = self.source
-                if news.url in all_news:
-                    print("News already in DB")
-                    if self.update:
-                        self.sr = self.max_result
-                        break
-                else:
-                    news.save()
-                self.sr += 1
-            except:
-                print(dt)
-
+            news = NewsItem()
+            news.title = n.xpath(".//h2[contains(@class,'c-headline__title')]")[0].text.strip()
+            news.text = n.xpath(".//p[contains(@class, 'c-headline__standfirst')]")[0].text_content().strip()
+            news.url = n.xpath(".//div[contains(@class, 'c-headline__content')]/a")[0].get("href")
+            dt = n.xpath(".//time")[0].text_content().strip().replace('º','')
+            news.content_date = parseDate(dt)
+            news.metamemo = self.metamemo[0]
+            news.source = self.source
+            if news.url in all_news:
+                print("News already in DB")
+                if not self.update:
+                    self.sr = self.max_result
+                    break
+            elif self.checkWords(self.filter_words, news.title):
+                print("Saving...")
+                news.save()
+            else:
+                print("Skipping..."+news.title)
+            
+            self.sr += 1
+            
     def add_arguments(self, parser):
         parser.add_argument('-k', '--keyword', type=str, help='Palavra-chave')
+        parser.add_argument('-f', '--filter', type=str, help='Filtra resultados com palavras no headline')
         parser.add_argument('-a', '--author', type=str, help='MetaMemo Author Name')
         parser.add_argument('-u', '--update', action='store_true')
 
 
     def handle(self, *args, **kwargs):
-        keyword = kwargs['keyword']
+        self.keyword = kwargs['keyword']
         self.author = kwargs['author']
+        self.filter_words = kwargs['filter']
         self.metamemo = MetaMemo.objects.get_or_create(name=self.author)
         self.source = NewsSource.objects.get_or_create(name="Folha de São Paulo")[0]
         self.update = kwargs['update']
 
         while self.sr < self.max_result:
             print("Starting from..." + str(self.sr))
-            self.scrapeFolha(keyword)
+            self.scrapeFolha(self.keyword)
