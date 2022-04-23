74de8c73aafd88e322c48feb2ceb49b429f91401
Pedro Markun <pedro@markun.com.br>
2022-04-23 15:45:33 -0300

    checa tamanho da lista no download_a_few

--- metamemoapp/management/commands/download_a_few.py
+++ metamemoapp/management/commands/download_a_few.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 
 from metamemoapp.models import MemoMedia
 from metamemoapp.tasks import download_img_async, download_async
+from metamemo.celery import app
 
 class Command(BaseCommand):
     help = 'Download media from a few random medias'
@@ -11,13 +12,19 @@ class Command(BaseCommand):
         parser.add_argument('-t', '--type', type=str, help='Which types?')
         parser.add_argument('-f', '--fake', action='store_true')
         parser.add_argument('-r', '--retry', action='store_true')
-
+        parser.add_argument('-q', '--queue', type=str, help='Queue to check')
     def handle(self, *args, **kwargs):
         self.count = kwargs['number']
         self.type = kwargs['type']
         self.fake = kwargs['fake']
         self.retry = kwargs['retry']
-        
+        self.queue = kwargs['queue']
+
+        if self.queue:
+            q = len(app.inspect().reserved()[self.queue]
+            if q==0:
+                return None
+
         self.medias = MemoMedia.objects.filter(media='').exclude(status='DOWNLOADING')
         if not self.retry:
             self.medias = self.medias.exclude(status='FAILED_DOWNLOAD')
