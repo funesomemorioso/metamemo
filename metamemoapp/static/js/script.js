b54cdf24d9655f4c7b3e8effe7752f108a9b6af0
tales shift <talesshift@gmail.com>
2022-06-29 16:56:15 -0300

    add dyamic build from JSON to timelines page

--- metamemoapp/static/js/script.js
+++ metamemoapp/static/js/script.js
@@ -1,6 +1,12 @@
 window.mobileCheck = window.innerWidth <= 601;
 $(document).ready(function () {
     $.when($.getJSON("http://127.0.0.1:8000/timeline/api/v1/fact/?limit=100000"),$.getJSON( "http://127.0.0.1:8000/timeline/api/v1/session/?limit=100000")).then(function(data,data_sessions){
+        const option = {
+            year: 'numeric',
+            month: ('long' || 'short' || 'numeric'),
+            day: 'numeric',
+        }
+        const locale = 'pt-br'
         all_timelines = data[0]["objects"].map(function(A) {return A["timeline"];});
         timelines = all_timelines.filter((value, index, self) =>
             index === self.findIndex((t) => (
@@ -64,7 +70,7 @@ $(document).ready(function () {
                 mes = data_t.toLocaleString("pt-BR", { month: "long" });
                 mesin = data_t.toLocaleString("pt-BR", { month: "short" });
                 ano = data_t.getFullYear()
-                console.log(`${dia} de ${mes} - ${ano}`)
+                //console.log(`${dia} de ${mes} - ${ano}`)
                 if(sess_id == -1){
 
                 }else{
@@ -83,14 +89,63 @@ $(document).ready(function () {
             $( "#ovo" ).after(`<section id="timeline-${this_tl_facts["name"].split(' ').join('-')}" class="timeline-body"></section>`)
             var sessions_list = []
             $.each( tl, function( sesh_key, this_sesh ) {
-                var sessio = `<div class="row timeline-board metagreen">
+                let ts_key = this_sesh["session"].replace(/^[^a-z]+|[^\w:.-]+/gi, "");
+                var sessio = `<div class="row timeline-board ${this_sesh["color"]}">
                     <div><b>${this_sesh["session"]}</b></div>
                     ${this_sesh["text"]}
                     <small>${new Date(this_sesh["start"]).getFullYear()}-${new Date(this_sesh["end"]).getFullYear()}</small>
                     </div>`;
-                sessions_list.push(sessio)
-            })
-            console.log(sessions_list.join( "" ))
+                var sessio_years = []
+                var sessio_modals = []
+                $.each( this_sesh["years"], function( y_key, this_year ) {
+                    ano_ini = `<div class="row">
+                    <span class="bubble ${this_sesh["color"]}">${y_key}</span>`
+                    ano_fim = `</div>` 
+                    this_year_months = []
+                    $.each(this_year,function( m_key, this_month ) {
+                        mesio = `
+                        <div class="row">
+                            <span data-target="modal-${(m_key.split('.').join(""))+"-"+y_key+"-"+ts_key}" class="bubble mini ${this_sesh["color"]} modal-trigger">${m_key.split('.').join("").toUpperCase()}</span>
+                        </div>
+                        `
+                        month_modal = [`
+                        <div id="modal-${(m_key.split('.').join(""))+"-"+y_key+"-"+ts_key}" class="modal timeline-modal" tabindex="0" style="z-index: 1003; display: none; opacity: 0; top: 4%; transform: scaleX(0.8) scaleY(0.8);">
+                            <div class="modal-content">
+                        `]
+                        $.each(this_month,function( f_key, this_fact ) {
+                            const option = {
+                                year: 'numeric',
+                                month: ('long' || 'short' || 'numeric'),
+                                day: 'numeric',
+                            }
+                            const locale = 'pt-br'
+                            data_str = new Date(this_fact["date"]).toLocaleDateString( locale, option)
+                            factio = `
+                            <p class="radius8 metapink white-text" title="source: ${this_fact["source"]} (${this_fact["resource_uri"]})"><b>${data_str}</b> - ${this_fact["text"]}</p>
+                            `
+                            month_modal.push(factio)
+                            if(this_fact["image"] != null){
+                                imagio =`<img src="${this_fact["image"]}" alt="" class="z-depth-3">`
+                                month_modal.push(imagio)
+                            }
+                        });
+                        month_modal.push(`</div>
+                        </div>`)
+                        this_year_months.push(mesio)
+                        sessio_modals.push(month_modal.join( "" ))
+                    })
+                    anio=(ano_ini+this_year_months.join( "" )+ano_fim)
+                    sessio_years.push(anio)
+                });
+                sessions_list.push(sessio+sessio_years.join( "" ))
+                $("section.no-padding-bottom").after(sessio_modals.join( "" ))
+            });
+            console.log("ovo")
+            console.log(this_tl_facts)
+            this_tl_destaque_image = ""
+            if(this_tl_facts["facts"][0]["image"]!= null){
+                this_tl_destaque_image = `<span class="bubble large metapink">${new Date(this_tl_facts["facts"][0]["date"]).getFullYear()}</span><img src="${this_tl_facts["facts"][0]["image"]}" alt="" class="z-depth-3"/>`
+            }
             $(`#timeline-${this_tl_facts["name"].split(' ').join('-')}`).append(`
             <div class="center-align">
                 <img src="https://picsum.photos/282" alt="" class="circle">
@@ -98,25 +153,15 @@ $(document).ready(function () {
             <div class="center-align metagreen-text font35 font26-s fontBold">
                 ${this_tl_facts["name"].toUpperCase()}
             </div>
+            <div class="start container center-align">
+                ${this_tl_destaque_image}
+                <p class="radius8 grey lighten-3"><b>${new Date(this_tl_facts["facts"][0]["date"]).toLocaleDateString( locale, option)}</b> - ${this_tl_facts["facts"][0]["text"]}</p>
+            </div>
             <div class="timeline-content">
                 ${sessions_list.join( "" )}
             </div>
             `);
-
-
-            header = `
-            <section id="timeline-jair-bolsonaro" class="timeline-body active">
-                <div class="center-align">
-                    <img src="https://picsum.photos/282" alt="" class="circle">
-                </div>
-                <div class="center-align metagreen-text font35 font26-s fontBold">
-                    ${this_tl_facts["name"].toUpperCase()}
-                </div>
-                <div class="timeline-content">
-            `
-
-
-
+            
         })  
 /*         $( "#ovo" ).after(`<section id="timeline-jair_bolsonaro" class="timeline-body active">
         <div class="center-align">
