660358eb422dee0a3b3d99dacbd63586a005dc75
tales shift <talesshift@gmail.com>
2022-06-29 12:28:54 -0300

    live
    Co-authored-by: Capi Etheriel <barraponto@gmail.com>

--- metamemoapp/static/js/script.js
+++ metamemoapp/static/js/script.js
@@ -1,10 +1,205 @@
 window.mobileCheck = window.innerWidth <= 601;
 $(document).ready(function () {
-    $.getJSON( "http://127.0.0.1:8000/timeline/api/v1/fact/?limit=100000", function( data ) {
-        $.each( data["objects"], function( key, val ) {
-            console.log(val)
+    $.when($.getJSON("http://127.0.0.1:8000/timeline/api/v1/fact/?limit=100000"),$.getJSON( "http://127.0.0.1:8000/timeline/api/v1/session/?limit=100000")).then(function(data,data_sessions){
+        all_timelines = data[0]["objects"].map(function(A) {return A["timeline"];});
+        timelines = all_timelines.filter((value, index, self) =>
+            index === self.findIndex((t) => (
+                t.place === value.place && t.name === value.name
+            ))
+        );
+        timeline_selectors = [];
+        facts = []
+        sessions = []
+        facts_timelines = []
+        $.each( timelines, function( key, val ) {            
+            nome_id = val["name"].split(' ').join('-');
+            timeline_selectors.push(`
+                <div class="col offset-s3 s6 m3 padding-top50 center-align">
+                    <a id="${nome_id}" class="white-text">
+                        <img src="https://picsum.photos/130" alt="" width="100%" class="circle darken-filter">
+                        <br>${val["name"]}
+                    </a>
+                </div>
+            `);
+            facts.push({timeline: val["id"],name:val["name"],facts:data[0]["objects"].filter(obj => {
+                return obj["timeline"]["id"] === val["id"]
+            })});
+            sessions.push({timeline: val["id"],sessions:data_sessions[0]["objects"].filter(obj => {
+                return obj["timeline"]["id"] === val["id"]
+            })});
         });
+        $( ".timeline-seletor" ).html(timeline_selectors.join( "" ));
+        
+        $.each(facts, function(this_tl_key,this_tl_facts){
+            //sort this timeline's facts
+            this_tl_facts["facts"].sort(function(a,b){
+                return new Date(a.date) - new Date(b.date);
+            });
+            this_tl_sessions = sessions[this_tl_key]
+            //sort this timeline's sessions
+            this_tl_sessions["sessions"].sort(function(a,b){
+                return new Date(a.start) - new Date(b.start);
+            });
+            console.log("oooooooo")
+            console.log(this_tl_sessions)
+            
+            //this timeline's header
+            
+            tl = [] // this_timeline's list of session objects with facts categorized by date inside 
+            //session colors list to loop over
+            session_cores = ["metablue","metagreen","metaroxo","metagold","metared"]
+            $.each(this_tl_sessions["sessions"],function(session_key,session){
+                //loop over colors
+                cor_atual = session_cores[session_key % session_cores.length]
+                //add session to TL 
+                tl.push({session:session["title"],text:session["text"],start:session["start"],end:session["end"],color:cor_atual,years:{}}) 
+            });
+            $.each(this_tl_facts["facts"],function(fact_key,fact){
+                let data_t = new Date(fact["date"])
+                function isAround(object){//checks if session encompases this date
+                    return data_t > new Date(object["start"]) && data_t <  new Date(object["end"]);
+                }      
+                sess_id = tl.findIndex(isAround);//finds the first session that encompasses this date
+                dia = data_t.getDate() 
+                mes = data_t.toLocaleString("pt-BR", { month: "long" });
+                mesin = data_t.toLocaleString("pt-BR", { month: "short" });
+                ano = data_t.getFullYear()
+                console.log(`${dia} de ${mes} - ${ano}`)
+                if(sess_id == -1){
+
+                }else{
+                    if(tl[sess_id]["years"][ano]===undefined){
+                        tl[sess_id]["years"][ano]={};
+                    }
+                    if(tl[sess_id]["years"][ano][mesin]===undefined){
+                        tl[sess_id]["years"][ano][mesin]=[];
+                    }
+                    tl[sess_id]["years"][ano][mesin].push(fact)
+                }
+            });
+            console.log("end_tl")
+            console.log(tl)
+            
+            $( "#ovo" ).after(`<section id="timeline-${this_tl_facts["name"].split(' ').join('-')}" class="timeline-body"></section>`)
+            var sessions_list = []
+            $.each( tl, function( sesh_key, this_sesh ) {
+                var sessio = `<div class="row timeline-board metagreen">
+                    <div><b>${this_sesh["session"]}</b></div>
+                    ${this_sesh["text"]}
+                    <small>${new Date(this_sesh["start"]).getFullYear()}-${new Date(this_sesh["end"]).getFullYear()}</small>
+                    </div>`;
+                sessions_list.push(sessio)
+            })
+            console.log(sessions_list.join( "" ))
+            $(`#timeline-${this_tl_facts["name"].split(' ').join('-')}`).append(`
+            <div class="center-align">
+                <img src="https://picsum.photos/282" alt="" class="circle">
+            </div>
+            <div class="center-align metagreen-text font35 font26-s fontBold">
+                ${this_tl_facts["name"].toUpperCase()}
+            </div>
+            <div class="timeline-content">
+                ${sessions_list.join( "" )}
+            </div>
+            `);
+
+
+            header = `
+            <section id="timeline-jair-bolsonaro" class="timeline-body active">
+                <div class="center-align">
+                    <img src="https://picsum.photos/282" alt="" class="circle">
+                </div>
+                <div class="center-align metagreen-text font35 font26-s fontBold">
+                    ${this_tl_facts["name"].toUpperCase()}
+                </div>
+                <div class="timeline-content">
+            `
+
+
+
+        })  
+/*         $( "#ovo" ).after(`<section id="timeline-jair_bolsonaro" class="timeline-body active">
+        <div class="center-align">
+            <img src="https://picsum.photos/282" alt="" class="circle">
+        </div>
+        <div class="center-align metagreen-text font35 font26-s fontBold">
+            JAIR MESSIAS BOLSONARO
+        </div>
+        <div class="start container center-align">
+            <span class="bubble large metapink">1888</span>
+            <img src="https://picsum.photos/670/375" alt="" class="z-depth-3">
+            <p class="radius8 grey lighten-3"><b>Abril de 1888</b> - Vittorio 
+    Bolsonaro emigra de Anguillara, na região do Vêneto, nordeste da Itália,
+     para o Brasil. Aos dez anos, viaja com o pai, a mãe e dois irmãos. 
+    Desembarcaram em Santos. Angelo, seu filho, casou com uma brasileira 
+    descendente de alemães e teve Percy Geraldo, pai de Jair Messias 
+    Bolsonaro em 1927.</p>
+        </div>
+        <div class="timeline-content">
+            <div class="row timeline-board metagreen">
+                <div><b>PARTE 1</b></div>
+                Jair, infância e adolescência 
+                <small>1955-1972</small>
+            </div>
+            <div class="row">
+                <span class="bubble metagreen">1955</span>
+                <div class="row">
+                    <span data-target="modal1" class="bubble mini metagreen modal-trigger">JAN</span>
+                </div>
+                <div class="row">
+                    <span data-target="modal1" class="bubble mini metagreen modal-trigger">FEV</span>
+                </div>
+            </div>
+            <div class="row"><span class="bubble metagreen">1955</span></div>
+            <div class="row"><span class="bubble metagreen">1955</span></div>
+    
+            <div class="row timeline-board metagold">
+                <div><b>PARTE 2</b></div>
+                Jair, de cadete à capitão
+                <small>1973-1988</small>
+            </div>
+            <div class="row"><span class="bubble metagold">1955</span></div>
+            <div class="row"><span class="bubble metagold">1955</span></div>
+            <div class="row"><span class="bubble metagold">1955</span></div>
+    
+            <div class="row timeline-board metablue"> 
+                <div><b>PARTE 3</b></div>
+                Jair, de vereador a deputado polêmico 
+                <small>1988- 2015</small>
+            </div>
+            <div class="row"><span class="bubble metablue">1955</span></div>
+            <div class="row"><span class="bubble metablue">1955</span></div>
+            <div class="row"><span class="bubble metablue">1955</span></div>
+    
+            <div class="row timeline-board metared">
+                <div><b>PARTE 4</b></div>
+                Jair, de vereador a deputado polêmico
+                <small>1988- 2015</small> 
+            </div>
+            <div class="row"><span class="bubble metared">1955</span></div>
+            <div class="row"><span class="bubble metared">1955</span></div>
+            <div class="row"><span class="bubble metared">1955</span></div>
+        </div>
+    </section>`); */
+        $('.timeline-seletor a').each(function(index, el) {
+            $(this).click(function(event) {
+                $('.timeline-body').removeClass('active');
+                $(`#timeline-${this.id}`).addClass('active');
+            });
+        });
+        $('.timeline-content > .row').each(function(index, el) {
+            var row = this;
+            $(row).find('> .bubble').click(function(event) {
+                $(row).toggleClass('active');
+            });
+        });
+        $('.modal').modal();
     });
+    
+/*     
+    $.getJSON( "http://127.0.0.1:8000/timeline/api/v1/fact/?limit=100000", function( data ) {
+
+    }); */
     $('.parallax').parallax();
     $('.resultado-slider .slider').slider({
         indicators: false,
@@ -154,18 +349,5 @@ $(document).ready(function () {
     });
 
     /*timeline*/
-    $('.timeline-seletor a').each(function(index, el) {
-        $(this).click(function(event) {
-            $('.timeline-body').removeClass('active');
-            $(`#timeline-${this.id}`).addClass('active');
-        });
-    });
-    $('.timeline-content > .row').each(function(index, el) {
-        var row = this;
-        $(row).find('> .bubble').click(function(event) {
-            $(row).toggleClass('active');
-        });
-    });
-    $('.modal').modal();
 
 });
