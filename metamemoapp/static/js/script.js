2c729f7f3da8b24ef16ec2b36980de85e872282a
tales shift <talesshift@gmail.com>
2022-07-30 12:21:49 -0300

    sloved initial search date parsing

--- metamemoapp/static/js/script.js
+++ metamemoapp/static/js/script.js
@@ -65,8 +65,9 @@ $(document).ready(function () {
             'source': sources,
             'content': $("#id_content").val()
         }
-
         var sd = M.Datepicker.getInstance($("#start_date")).date;
+        console.log("alou")
+        console.log(sd)
         var ed = M.Datepicker.getInstance($("#end_date")).date;
 
         if (sd) {
@@ -85,6 +86,7 @@ $(document).ready(function () {
     //Datepicker
     $('.datepicker').datepicker({
         yearRange: [2008, 2022],
+        defaultDate: new Date(Date.parse(this.value)),
         i18n: {
             today: 'Hoje',
             clear: 'Limpar',
@@ -97,6 +99,10 @@ $(document).ready(function () {
             monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
             months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
         }
+    });
+    jQuery('.datepicker').each(function () {
+        var newDate = new Date(Date.parse(this.value));
+        $(this).datepicker('setDate', newDate);
     });
 
 
