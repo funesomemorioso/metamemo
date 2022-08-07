fe50f4fffe6d7ac4e7b6ac23e707e86b918e3817
Pedro Markun <pedro@markun.com.br>
2022-08-07 11:53:07 -0300

    removendo antigas funções de busca

--- metamemoapp/views.py
+++ metamemoapp/views.py
@@ -195,55 +195,6 @@ def lista(request):
     return render(request, "files.html", {"data": data})
 
 
-def search(request, year=None, month=None, day=None):
-    if not year:
-        year = datetime.now().year
-        month = datetime.now().month
-        day = datetime.now().day
-
-    date = datetime(year, month, day)
-    end_date = date + timedelta(1)
-
-    memoqs = MemoItem.objects.filter(
-        content_date__gte=date, content_date__lte=end_date
-    ).order_by("content_date")
-    memofilter = MemoItemFilter(request.GET, queryset=memoqs)
-
-    newsqs = NewsItem.objects.filter(
-        content_date__gte=date, content_date__lte=end_date
-    ).order_by("content_date")
-    newsfilter = MemoNewsFilter(request.GET, queryset=newsqs)
-
-    memoitem = Paginator(memofilter.qs, 50)
-    memonews = Paginator(newsfilter.qs, 50)
-
-    memocontext = MemoContext.objects.filter(
-        start_date__lte=date, end_date__gte=date
-    ).order_by("start_date")
-    newscover = NewsCover.objects.filter(
-        content_date__gte=date, content_date__lte=end_date
-    )
-
-    # Hackish
-    tags = {}
-    tags_raw = memofilter.qs.values_list("keyword__word", flat=True)
-    tags = Counter(tags_raw)
-    tags[None] = 0
-
-    data = {
-        "memofilter": memofilter,
-        "metamemo": MetaMemo.objects.all(),
-        "memocontext": memocontext,
-        "memoitem": memoitem.page(1),
-        "memonews": memonews.page(1),
-        "newscovers": newscover,
-        "tags": tags.most_common(10),
-        "date": date,
-    }
-
-    return render(request, "search.html", {"data": data})
-
-
 def memoitem(request, item_id):
     memoitem = MemoItem.objects.get(pk=item_id)
     return render(request, "memoitem.html", {"memoitem": memoitem})
