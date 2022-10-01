ca167ae83219ad3eb9fe5b03e1efdd212d205a07
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2022-09-30 21:41:48 -0300

    Configure whitenoise + use gunicorn on dev

--- bin/web.sh
+++ bin/web.sh
@@ -7,24 +7,24 @@ set -o nounset
 if [ -z ${PORT+x} ]; then
 	PORT=5000
 fi
-HOST_PORT="0.0.0.0:$PORT"
 if [ -z ${USE_ASGI+x} ]; then
 	USE_ASGI=false
 fi
-
-if [[ $(echo $DEV_BUILD | tr a-z A-Z) = "TRUE" ]]; then
-	python manage.py runserver $HOST_PORT
-else
-	if [ -z ${WEB_WORKERS+x} ]; then
-		WEB_WORKERS=4
-	fi
-
-	OPTS="--bind=$HOST_PORT --chdir=/app --log-file - --workers=$WEB_WORKERS"
-	if [[ "$(echo $USE_ASGI | tr a-z A-Z)" = "TRUE" ]]; then
-		APP_MODULE="metamemo.asgi:application"
-		OPTS="$OPTS --worker-class uvicorn.workers.UvicornWorker"
-	else
-		APP_MODULE="metamemo.wsgi:application"
-	fi
-	gunicorn $OPTS $APP_MODULE
+if [ -z ${WEB_WORKERS+x} ]; then
+	WEB_WORKERS=4
 fi
+
+HOST_PORT="0.0.0.0:$PORT"
+OPTS="--bind=$HOST_PORT --chdir=/app --log-file - --workers=$WEB_WORKERS"
+if [[ "$(echo $USE_ASGI | tr a-z A-Z)" = "TRUE" ]]; then
+	APP_MODULE="metamemo.asgi:application"
+	OPTS="$OPTS --worker-class uvicorn.workers.UvicornWorker"
+else
+	APP_MODULE="metamemo.wsgi:application"
+fi
+if [[ $(echo $DEV_BUILD | tr a-z A-Z) = "TRUE" ]]; then
+	OPTS="$OPTS --reload"
+fi
+
+python manage.py collectstatic --no-input
+gunicorn $OPTS $APP_MODULE
--- metamemo/settings.py
+++ metamemo/settings.py
@@ -64,6 +64,7 @@ if DEBUG:
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
