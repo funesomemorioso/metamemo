6045675dac29a91b677da633c008ba140131c9ff
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2022-11-25 17:04:46 -0300

    Little adjustments in admin config + timezone

--- metamemo/settings.py
+++ metamemo/settings.py
@@ -149,7 +149,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 LANGUAGE_CODE = "pt-br"
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
