8f6ffad5871f1b9ae830404cf5817aed8af3a540
Marcel Marques <marcmatiasdev@gmail.com>
2023-04-05 11:05:06 -0300

    Updating script with set folder name arg option

--- copy-media.sh
+++ copy-media.sh
@@ -6,8 +6,12 @@
 
 filename="$1"
 
+defaultfoldername="media"
+foldername="${2:-$defaultfoldername}"
+
 if [[ -z $filename ]]; then
- echo "ERROR - Usage: $0 <filename>"
+ echo "ERROR - Usage: $0 <filename> <foldername>"
+ exit 1
 fi
 
 bucket=$AWS_STORAGE_BUCKET_NAME
@@ -16,17 +20,17 @@ host=$(echo $AWS_S3_ENDPOINT_URL | sed 's/https\?:\/\///; s/\/.*//')
 s3_key=$AWS_S3_ACCESS_KEY_ID
 s3_secret=$AWS_S3_SECRET_ACCESS_KEY
 
-mkdir -p media
-rm -rf "media/${filename}"
-curl --output "media/${filename}" https://storage.metamemo.info/${bucket}/media/${filename}
+mkdir -p $foldername
+rm -rf "${foldername}/${filename}"
+curl --output "${foldername}/${filename}" https://storage.metamemo.info/${bucket}/${foldername}/${filename}
 
-resource="/${bucket}/media/${filename}"
+resource="/${bucket}/${foldername}/${filename}"
 content_type="application/octet-stream"
 date=`date -R`
 _signature="PUT\n\n${content_type}\n${date}\n${resource}"
 signature=`echo -en ${_signature} | openssl sha1 -hmac ${s3_secret} -binary | base64`
 
-curl -X PUT -T "media/${filename}" \
+curl -X PUT -T "${foldername}/${filename}" \
           -H "Host: ${host}" \
           -H "Date: ${date}" \
           -H "Content-Type: ${content_type}" \
