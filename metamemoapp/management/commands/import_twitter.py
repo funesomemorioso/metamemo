33fbfb16b52e2b86f388afe76db9681d044d9cae
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2023-07-03 13:14:07 -0300

    Update Twitter scraper to get data from all MetaMemos

--- metamemoapp/management/commands/import_twitter.py
+++ metamemoapp/management/commands/import_twitter.py
@@ -1,4 +1,5 @@
 import json
+import time
 from textwrap import shorten
 
 import tweepy
@@ -12,43 +13,37 @@ from metamemoapp.tasks import download_async, download_img_async
 
 TITLE_MAX_CHAR = 300
 
-"""
-Cada arquivo dentro de management/commands é um comando que pode ser rodado usando 'python manage.py nomedocomando'.
-Handle() é a função executada pelo código.
-A vantagem de fazer assim é que as libs e dependencias do Django já estão encapsuladas e você pode chamar essa função dentro do resto do código.
-"""
-
-
 class Command(BaseCommand):
     help = "Importa de um usuário do twitter"
 
     def add_arguments(self, parser):
-        parser.add_argument("-u", "--username", type=str, help="Twitter Username")
-        parser.add_argument("-a", "--author", type=str, help="MetaMemo Author Name")
         parser.add_argument("-m", "--media", action="store_true")
         parser.add_argument("-r", "--refresh", action="store_true")
+        parser.add_argument("-w", "--wait-time", type=int, default=60, help="Time to wait between each MetaMemo")
 
     def handle(self, *args, **kwargs):
-        self.username = kwargs["username"]
-        self.author = kwargs["author"]
         self.video_download = kwargs["media"]
         self.update = kwargs["refresh"]
-        self.finished = False
-
+        self.wait_time = kwargs["wait_time"]
         if not settings.TWITTER_BEARER_TOKEN:
-            print("You need to setup the bearer token in .env")
-            raise
+            raise RuntimeError("You need to setup Twitter bearer token")
 
-        self.memo_author = MetaMemo.objects.get_or_create(name=self.author)
-        self.memo_source = MemoSource.objects.get_or_create(name="Twitter")
-        self.memo_itens = MemoItem.objects.filter(author__name=self.author, source__name="Twitter").values_list(
-            "original_id", flat=True
-        )
-        self.client = tweepy.Client(settings.TWITTER_BEARER_TOKEN)
-        self.user = self.client.get_user(username=self.username)
-        self.getTweets()
+        self.memo_source, _ = MemoSource.objects.get_or_create(name="Twitter")
+        for metamemo in MetaMemo.objects.all():
+            if not metamemo.twitter_handle:
+                continue
+            print(f"Collecting Twitter for {metamemo.twitter_handle}")
+            self.username = metamemo.twitter_handle
+            self.memo_itens = MemoItem.objects.filter(author=metamemo, source=self.memo_source).values_list(
+                "original_id", flat=True
+            )
+            self.client = tweepy.Client(settings.TWITTER_BEARER_TOKEN)
+            self.user = self.client.get_user(username=self.username)
+            self.getTweets()
+            time.sleep(self.wait_time)
 
     def getTweets(self, paginationToken=None):
+        finished = False
         input_posts = self.client.get_users_tweets(
             self.user.data.id,
             media_fields=["url", "type", "media_key"],
@@ -58,13 +53,10 @@ class Command(BaseCommand):
             pagination_token=paginationToken,
         )
 
-        """
-        Aqui estou usando o MemoItem() para instancear um objeto que defini lá no models.
-        E a função save() para salvar efetivamente no banco.
-        Tem um outro jeito de fazer onde você comita todas as alterações e salva de uma vez, acho que vale refatorar depois.
-
-        """
-
+        # Aqui estou usando o MemoItem() para instancear um objeto que defini
+        # lá no models.  E a função save() para salvar efetivamente no banco.
+        # Tem um outro jeito de fazer onde você comita todas as alterações e
+        # salva de uma vez, acho que vale refatorar depois.
         tweet_media = {}
 
         if "media" in input_posts.includes:
@@ -77,12 +69,12 @@ class Command(BaseCommand):
         for i in input_posts.data:
             if str(i.id) in self.memo_itens:
                 if not self.update:
-                    self.finished = True
+                    finished = True
                     break
             else:
                 post = MemoItem()
-                post.author = self.memo_author[0]
-                post.source = self.memo_source[0]
+                post.author = self.memo_author
+                post.source = self.memo_source
                 post.title = shorten(i.text, TITLE_MAX_CHAR)
                 post.content = i.text
                 post.extraction_date = timezone.now()
@@ -106,7 +98,7 @@ class Command(BaseCommand):
                                 original_id=m,
                                 status="INITIAL",
                                 mediatype="VIDEO",
-                                source=self.memo_source[0],
+                                source=self.memo_source,
                             )
                             if self.video_download:
                                 p.status = "DOWNLOADING"
@@ -123,5 +115,5 @@ class Command(BaseCommand):
                             post.save()
                             download_img_async.apply_async(kwargs={"url": media["url"], "pk": p.pk}, queue="twitter")
 
-        if "next_token" in input_posts.meta and not self.finished:
+        if "next_token" in input_posts.meta and not finished:
             self.getTweets(paginationToken=input_posts.meta["next_token"])
