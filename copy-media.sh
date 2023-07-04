b1d3409041cf89ed9e47fca42f456f4ce81da1f3
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2023-07-03 22:17:03 -0300

    Fix copy-media script

--- copy-media.sh
+++ copy-media.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # This script:
-# - Downloads $filename from storage.metamemo.info
+# - Downloads $filename from storage.metamemo.org.br
 # - Uploads file to local MinIO
 
 filename="$1"
 
-defaultfoldername="media"
-foldername="${2:-$defaultfoldername}"
-
 if [[ -z $filename ]]; then
- echo "ERROR - Usage: $0 <filename> <foldername>"
+ echo "ERROR - Usage: $0 <filename>"
  exit 1
 fi
 
@@ -20,17 +17,18 @@ host=$(echo $AWS_S3_ENDPOINT_URL | sed 's/https\?:\/\///; s/\/.*//')
 s3_key=$AWS_S3_ACCESS_KEY_ID
 s3_secret=$AWS_S3_SECRET_ACCESS_KEY
 
-mkdir -p $foldername
-rm -rf "${foldername}/${filename}"
-curl --output "${foldername}/${filename}" https://storage.metamemo.info/${bucket}/${foldername}/${filename}
+full_filename="/tmp/mtm-tmp/${filename}"
+mkdir -p $(dirname $full_filename)
+rm -rf "$full_filename"
+curl --output "$full_filename" https://storage.metamemo.org.br/${bucket}/${filename}
 
-resource="/${bucket}/${foldername}/${filename}"
+resource="/${bucket}/${filename}"
 content_type="application/octet-stream"
 date=`date -R`
 _signature="PUT\n\n${content_type}\n${date}\n${resource}"
 signature=`echo -en ${_signature} | openssl sha1 -hmac ${s3_secret} -binary | base64`
 
-curl -X PUT -T "${foldername}/${filename}" \
+curl -X PUT -T "$full_filename" \
           -H "Host: ${host}" \
           -H "Date: ${date}" \
           -H "Content-Type: ${content_type}" \
